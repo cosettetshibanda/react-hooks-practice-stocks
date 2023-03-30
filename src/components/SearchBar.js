2b260bec7945469c0d0fc235e0875324e84e81f9
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-03-30 13:55:00 -0600

    commit tree a24269912828df9d637774ae55e476f99a5e998b

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({sortStocks, sortBy, handleFilterChange}) {
+
+  function onFilterChange(event) {
+    handleFilterChange(event.target.value);
+  }
+
   return (
     <div>
       <strong>Sort by:</strong>
@@ -9,8 +14,8 @@ function SearchBar() {
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={ sortBy === "Alphabetically"}
+          onChange={sortStocks}
         />
         Alphabetically
       </label>
@@ -19,15 +24,15 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={ sortBy === "Price"}
+          onChange={sortStocks}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={onFilterChange}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
